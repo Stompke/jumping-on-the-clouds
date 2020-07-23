5d56d5ec0a228ee18c5da34c97bbfec8685036f8
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-07-23 09:29:05 -0700

    added challenge link

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
 def jumpingOnClouds(c):
     print(c)
     count = 0
